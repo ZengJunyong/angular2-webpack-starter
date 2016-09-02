82ab33681bce4f72a2e85e53a72ff37018129db5
yong <zengjunyong@gmail.com>
2016-09-02 11:00:39 +0800

    move styles.css into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
-import {Component} from '@angular/core';
+import {Component,ViewEncapsulation} from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './styles.css'
+  ],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
